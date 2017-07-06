c1491544c9b88a3419832d856befc3187465fc75
akavick <akavick@tut.by>
2017-07-06 21:36:11 +0300

    1

---
+++ HomeWork001/DiskInfoSolutionWithoutWCFCL/Library/IMyDiskInfoServer.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+
+namespace Library
+{
+    [ServiceContract]
+    public interface IMyDiskInfoServer
+    {
+        [OperationContract]
+        string GetFreeSpace(string diskName);
+
+        [OperationContract]
+        string GetTotalSpace(string diskName);
+    }
+}
---
+++ HomeWork001/DiskInfoSolutionWithoutWCFCL/MyDiskInfoChannelClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using Library;
+
+namespace MyDiskInfoChannelClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<IMyDiskInfoServer> factory = null;
+
+            try
+            {
+                Console.Title = "ChannelClient";
+                Task.Delay(1000).Wait();
+                factory = new ChannelFactory<IMyDiskInfoServer>("MyDiskInfoServerEndPoint");
+                IMyDiskInfoServer channel = factory.CreateChannel();
+
+                while (true)
+                {
+                    Console.WriteLine("Выберите действие или CTRL+C для выхода");
+                    Console.WriteLine("\t1 - получить объём свободного места на диске");
+                    Console.WriteLine("\t2 - получить объём всего места на диске");
+                    var choice = Console.ReadKey(true).KeyChar;
+                    if (choice == '1' || choice == '2')
+                    {
+                        Console.WriteLine("Введите запрос или CTRL+C для выхода");
+                        var query = Console.ReadLine();
+                        var answer = choice == '1' ? channel.GetFreeSpace(query) : channel.GetTotalSpace(query);
+                        Console.WriteLine(answer);
+                    }
+                    else Console.WriteLine("Некорректный ввод");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                if (factory != null)
+                    factory.Close();
+            }
+        }
+    }
+}
